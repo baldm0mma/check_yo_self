017120ba0ac3ebdae93bed2efc729f8003032383
Jev Forsberg <46619047+baldm0mma@users.noreply.github.com>
2019-04-15 17:33:05 -0600

    Revert "Finish phase 2"

--- todo_list.js
+++ todo_list.js
@@ -20,15 +20,10 @@ class Card {
 
   updateToDos() {
     this.urgent = !this.urgent;
-    this.saveToStorage(); 
   }
 
   updateTask(index) {
-    this.taskList[index].done = !this.taskList[index].done;
-    this.taskList[index].done ? 
-      this.taskList[index].doneImg = 'images/checkbox-active.svg' : 
-      this.taskList[index].doneImg = 'images/checkbox.svg';
-    this.saveToStorage();
+    this.taskList[index].done = !this.done;
   } 
 }
 
@@ -37,6 +32,5 @@ class Items {
     this.content = content;
     this.done = false;
     this.id = Date.now();
-    this.doneImg;
   }
 }
